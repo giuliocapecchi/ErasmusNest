bcf9c8551e73ffb34ed994d26e5e759e9c4df3a1
JacopoNiccolai <jacoponiccolai99@gmail.com>
2024-01-20 19:21:06 +0100

    MyReservationsController fixed

--- src/main/java/it/unipi/erasmusnest/ErasmusNestApplication.java
+++ src/main/java/it/unipi/erasmusnest/ErasmusNestApplication.java
@@ -14,8 +14,7 @@ public class ErasmusNestApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        FXMLLoader fxmlLoader = new FXMLLoader(ErasmusNestApplication.class.getResource("myProfile-view.fxml"));
-        //FXMLLoader fxmlLoader = new FXMLLoader(ErasmusNestApplication.class.getResource("login-view.fxml"));
+       FXMLLoader fxmlLoader = new FXMLLoader(ErasmusNestApplication.class.getResource("login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 700,550);
         stage.setMinWidth(700);
         stage.setMinHeight(550);
