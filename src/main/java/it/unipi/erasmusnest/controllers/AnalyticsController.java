c2ad8f685dfeaec5f2826f18309dcb617d630499
Giulio Capecchi <giuliocapecchi2000@gmail.com>
2024-02-09 15:41:39 +0100

    small correction

--- src/main/java/it/unipi/erasmusnest/controllers/AnalyticsController.java
+++ src/main/java/it/unipi/erasmusnest/controllers/AnalyticsController.java
@@ -66,7 +66,7 @@ public class AnalyticsController extends Controller {
         vboxPriceOutput.prefWidthProperty().bind(super.getRootPane().widthProperty().multiply(0.4));
         vboxPositionOutput.prefWidthProperty().bind(super.getRootPane().widthProperty().multiply(0.4));
         hboxPriceButton.prefWidthProperty().bind(super.getRootPane().widthProperty().multiply(0.1));
-        citySplitComboBox.getItems().addAll(getNeo4jConnectionManager().getAllCities());
+        citySplitComboBox.getItems().addAll(getSession().getCities());
         citySplitComboBox.getItems().add("None");
         distanceSlider.valueProperty().addListener((observable, oldValue, newValue) ->
                 valueLabel.setText("Chosen distance from center: "+String.format("%.2f", newValue.doubleValue())+" km"));
