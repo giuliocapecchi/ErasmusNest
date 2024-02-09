f35710d3045583cd28f3af2e8949f371e6fa1b2a
Giulio Capecchi <giuliocapecchi2000@gmail.com>
2024-02-09 15:39:35 +0100

    added Help button

--- src/main/java/it/unipi/erasmusnest/controllers/HomepageController.java
+++ src/main/java/it/unipi/erasmusnest/controllers/HomepageController.java
@@ -1,10 +1,14 @@
 package it.unipi.erasmusnest.controllers;
 
+import it.unipi.erasmusnest.graphicmanagers.AlertDialogGraphicManager;
 import it.unipi.erasmusnest.model.User;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
+import org.controlsfx.control.PopOver;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +16,8 @@ import java.util.Objects;
 
 public class HomepageController extends Controller{
 
+    @FXML
+    Button helpButton;
     @FXML
     Button searchUserButton;
 
@@ -108,8 +114,6 @@ public class HomepageController extends Controller{
             loginButton.setVisible(true);
             loginButton.setManaged(true);
         }
-
-
     }
 
     private void updateSearchResults(String searchText) {
@@ -197,4 +201,22 @@ public class HomepageController extends Controller{
         }
         updateSearchResults(searchText);
     }
+
+    public void handleHelpAction() {
+        String title = "User guide";
+        String header = "Welcome to the user guide";
+        String content = "Welcome to the ErasmusNest application!\n\n" +
+                "Are you in search of the perfect apartment? Or perhaps you're looking to share your property with students from around the globe?\n" +
+                "Well, you've come to the right place!\n"+
+                "For students, we offer a wide range of homes and apartments." +
+                "\nWith our review system and personalized recommendations based on likes, you can discover the best destinations and\naccommodations tailored to your preferences and interests.\n"+
+                "For hosts, we provide a simple but efficient way to expose your\nproperty on the market. Manage your bookings, check your\nreviews, and be ready to welcome erasmus students from\n around the globe.\n"+
+                "\nYou can also connect with other users, follow their activities, and receive personalized recommendations. Whether it's advice based on your cities of interest, users to follow, or the most popular homes, we're here to help you create unforgettable experiences.\n"+
+                "Check your profile to update all your personal preferences."+
+                "\n\nWe hope you enjoy your stay with us!";
+        String type = "information";
+        AlertDialogGraphicManager alertDialogGraphicManager = new AlertDialogGraphicManager(title, header, content, type);
+        alertDialogGraphicManager.showAndGetConfirmation();
+    }
+
 }
